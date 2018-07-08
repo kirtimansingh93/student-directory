c66efa6040f8fc1f3c2c25c108220c9679e435a2
Kirtiman Singh <kirt93@hotmail.com>
2018-07-08 20:26:23 +0100

    more refactoring of interactive menu

--- directory.rb
+++ directory.rb
@@ -1,20 +1,50 @@
+@students = [] # an empty array accessible to all methods
+
 def input_students
   puts "Please enter the name of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
-  students = []
   # get the first name
   name = gets.chomp
   # while the name is not empty,repeat this code
   while !name.empty? do
     # add the student hash to the array
-    students << {name: name , cohort: :november}
-    puts "Now we have #{students.count} students"
+    @students << {name: name , cohort: :november}
+    puts "Now we have #{@students.count} students"
     # get another name from the user
     name = gets.chomp
   end
-  # return the array of input_students
-  students
+end
+
+def interactive_menu
+  loop do # 4. Repeat from step 1
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" # 9 because we'll be adding more items
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+    when "1" # Input the students
+      input_students
+    when "2" # Show the students
+      show_students
+    when "9"
+      exit # This will cause the program to terminate
+    else
+      puts "I don't know what you meant, try again."
+    end
 end
 
 def print_header
@@ -22,41 +52,14 @@ def print_header
   puts "-------------"
 end
 
-def print(students)
-  students.each_with_index do |student, index|
-    puts "#{index + 1}.#{student[:name]} (#{student[:cohort]} cohort)"
+def print_students_list
+  @students.each_with_index do |student, index|
+    puts "#{index + 1}) #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students."
-end
-
-def interactive_menu
-  students = [] # declare the variable students before the loop, setting
-  # it to an empty array so that it is available during several iterations
-  # of the loop.
-  loop do # 4. Repeat from step 1
-    # 1. Print menu and ask students what to do.
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit" # 9 becasue we will be adding more items
-    # 2. Read the input and save it into a vairiable.
-    selection = gets.chomp
-    # 3. Do what the user has asked.
-    case selection
-    when "1" # Input the students
-      students = input_students
-    when "2" # Show the students
-      print_header
-      print(students)
-      print_footer(students)
-    when "9"
-      exit # This will cause the program to terminate
-    else
-      puts "I don't know what you meant, try again."
-    end
-  end
+def print_footer
+  puts "Overall, we have #{@students.count} great students."
 end
 
 interactive_menu()
